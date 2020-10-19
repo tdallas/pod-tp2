e0127b276f47cc0b1c9f32b81d6f366fc6d93c10
tdallas <tdallas@itba.edu.ar>
2020-10-19 20:06:25 -0300

    feat: added basic hazelcast server and client working fine

--- client/src/main/java/itba/pod/client/Client.java
+++ client/src/main/java/itba/pod/client/Client.java
@@ -1,12 +1,24 @@
 package itba.pod.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
         logger.info("tpe2-g9-parent Client Starting ...");
+        ClientConfig config = new ClientConfig();
+        config.setInstanceName("dev");
+        HazelcastInstance hazelcastInstanceClient = HazelcastClient.newHazelcastClient(config);
+        Map<Long, String> map = hazelcastInstanceClient.getMap("data");
+        for (Map.Entry<Long, String> entry : map.entrySet()) {
+	        System.out.println(entry.getKey() + entry.getValue());
+        }
     }
 }
--- server/src/main/java/itba/pod/server/Server.java
+++ server/src/main/java/itba/pod/server/Server.java
@@ -1,12 +1,22 @@
 package itba.pod.server;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         logger.info("tpe2-g9-parent Server Starting ...");
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance();
+        Map<Long, String> map = hzInstance.getMap("data");
+        for (int i = 0; i < 10; i++) {
+            map.put((long) i, "message" + i);
+        }
+
     }
 }
