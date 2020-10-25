f91dbb5c011879a6cee10a3be46a0e4eb0bd2148
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2020-10-25 15:06:34 -0300

    [WIP] Nothing to see here

--- client/src/main/java/itba/pod/client/utils/ArgumentValidator.java
+++ client/src/main/java/itba/pod/client/utils/ArgumentValidator.java
@@ -15,6 +15,12 @@ public class ArgumentValidator {
         validatePositiveInteger(num);
     }
 
+    public static void validate(String addresses, String city, String inPath, String outPath, String minTreesString, String species) throws InvalidArgumentException {
+        validate(addresses, city, inPath, outPath);
+        validatePositiveInteger(minTreesString);
+        validateNotNull("-Dname", species);
+    }
+
     private static void validateNotNull(String argumentName, String argument) throws InvalidArgumentException {
         if (argument == null)
             throw new InvalidArgumentException(argumentName + " argument is required");
@@ -22,14 +28,17 @@ public class ArgumentValidator {
 
     private static void validateCity(String city) throws InvalidArgumentException {
         validateNotNull("-Dcity", city);
+
         if (!city.equals("BUE") && !city.equals("VAN"))
             throw new InvalidArgumentException("-Dcity should be BUE or VAN");
     }
 
     private static void validatePositiveInteger(String num) throws InvalidArgumentException {
         validateNotNull("number", num);
+
         try {
-            Integer integer = Integer.valueOf(num);
+            int integer = Integer.parseInt(num);
+
             if (integer < 0) throw new InvalidArgumentException("number should be a positive integer");
         } catch (NumberFormatException e) {
             throw new InvalidArgumentException("number should be a positive integer");
